fa1910c3b812eac5373ed92a983d6d2b84fe4dd2
Apokoala <michael_gazaway@hotmail.com>
2022-06-27 22:17:13 -0400

    fixed something i hope

--- apps.js
+++ apps.js
@@ -15,11 +15,11 @@ let message ='';
 
 if (firstName.includes (1)||(2)||(3)||(4)||(5)||(6)||(7)||(8)||(9)||(0)){
     // console.log("I see you have silent numbers in your name, "+ firstName +" tell your father his electric cars suck");
-    message = "I see you have silent numbers in your name, "+ firstName +" tell your father his electric cars suck";
+    message = "I see you have silent numbers in your name, "+ firstName +", tell your father his electric cars suck";
 }   else if (firstName.includes("m")){
     // console.log("YOUR NAME CONTAINS THE LETTER M!!!! "+ firstName +" THIS ANGERS ME");
     message = ("YOUR NAME CONTAINS THE LETTER M!!!! "+ firstName +" ...THIS ANGERS ME!!!");
-}   else if(firstName){
+}   else{
     // console.log("Congratulations, "+ firstName +" ,on having a name...I have a name too!");
     message = ("Congratulations, "+ firstName +" ,on having a name...I have a name too!");
 }
@@ -30,6 +30,13 @@ let result = confirm("Are you sure you want to be here?");
 let message2 = result ? 'Nice...' : 'I didn\'t want you here, anyway';
 
 alert(message2)
+
+
+
+
+
+
+
 // // output to the console some value
 // console.log('hello, ' + firstName)
 // console.log();
